00096bce7e64c83136fc1ca5f9a65bc5cf6fefd3
Rama Aditya <46771792+elcodee@users.noreply.github.com>
2021-05-22 17:49:26 +0700

    Add Week 3 Task

--- assets/js/week3/sentence-selection.js
+++ assets/js/week3/sentence-selection.js
@@ -4,10 +4,23 @@ document.getElementById("search").addEventListener("click", function () {
    
    let myWord = document.getElementById("word").value;
    let mySentence = document.getElementById("sentence").value.toLowerCase().split('.');
+   let display = [];
 
    for (let i = 0; i < mySentence.length; i++){
       if (mySentence[i].includes(myWord)) {
-         document.getElementById("display").innerHTML += mySentence[i] + ', ';
+         display.push(mySentence[i])
       }
    }
+
+   document.getElementById("display").innerHTML = `
+   <div class="alert alert-success alert-dismissible fade show" role="alert">
+      <strong>Output :</strong> ${display}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+   </div>`;;
+});
+ 
+document.getElementById("reset").addEventListener("click", function () {
+   
+   document.getElementById("display").innerHTML = '';
+
  }); 
